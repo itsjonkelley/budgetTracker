31181f75d9179e31c1450b2f1d78c55e8ee605bb
Jonathan Kelley <jonathankelley87@gmail.com>
2020-07-30 21:39:35 -0400

    update to online accessibility

--- public/index.js
+++ public/index.js
@@ -144,10 +144,11 @@ function sendTransaction(isAdding) {
   });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(true);
-};
-
-document.querySelector("#sub-btn").onclick = function() {
+});
+document.querySelector("#sub-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(false);
-};
+});
